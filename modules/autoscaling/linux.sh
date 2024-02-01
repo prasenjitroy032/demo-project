0afb8783da3b68531176ee922391e7d741d81fe8
Prasenjit Roy <prasenjit.roy032@gmail.com>
2024-02-01 19:48:44 +0530

    added lb modules updates fix_11

--- modules/autoscaling/linux.sh
+++ modules/autoscaling/linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-yum install -y ansible git
+yum install -y git
+pip install ansible
 git clone https://github.com/prasenjitroy032/demo-project.git /tmp/ansible-repo
+mkdir -p  /etc/ansible
+touch /etc/ansible/hosts
 echo '[localhost]' > /etc/ansible/hosts
 echo 'localhost ansible_connection=local' >> /etc/ansible/hosts
-ansible-playbook  /tmp/ansible-repo/ansible_httpd.yml
+ansible-playbook  -i /etc/ansible/hosts /tmp/ansible-repo/ansible_httpd.yml
