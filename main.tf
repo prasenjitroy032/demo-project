22658be443a3372f247c34b792a15b082da4a05f
Prasenjit Roy <prasenjit.roy032@gmail.com>
2024-02-01 12:51:30 +0530

    added lb modules updates

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   source             = "./modules/vpc"
   vpc_cidr           = "10.0.0.0/16" # Replace with your desired CIDR
   vpc_name           = "my-vpc"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
   public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"] 
 }
